f17777cd6a6f7e6f046361ade58581923ad502f2
Umesh Poswal <umesh.poswal@smartnews.com>
2024-09-03 23:34:04 +0900

    output a named file

--- daily-blog-creator.sh
+++ daily-blog-creator.sh
@@ -3,8 +3,21 @@ set -eu
 # Get the current date in the format yyyymmdd
 current_date=$(date +%F)
 
+# Check if at least one argument is provided
+if [ $# -lt 1 ]; then
+  echo "Blog name is required. Usage: cmd blog_name"
+  exit 1
+fi
+
+# Combine all positional parameters as the blog name
+blog_name="$*"
+
+title_blog_name=$(echo "$blog_name" | awk '{for(i=1;i<=NF;i++) $i=toupper(substr($i,1,1)) tolower(substr($i,2));} 1')
+snake_blog_name=$(echo "$blog_name" | tr ' ' '-' | tr '[:upper:]' '[:lower:]')
+
 # Define the file name
-file_name="journal/${current_date}-daily-journal.md"
+file_name="journal/${current_date}-${snake_blog_name}.md"
+
 # Check if the file already exists
 if [ -e "$file_name" ]; then
   echo "File '$file_name' already exists. Aborting."
@@ -13,8 +26,8 @@ fi
 
 # Create the content
 content="---
-title: ${current_date} <Change ME>
-slug: $(date +%Y)/<change me>
+title: ${current_date} ${title_blog_name}
+slug: $(date +%Y)/${snake_blog_name}
 tags:
 - '$(date +%Y-%m)'
 - '$(date +%Y)'
